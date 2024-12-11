a83bd492648294e1c488c5024c4d34056f9e750c
jatebezi <jempire47@gmial.com>
2024-12-11 22:35:50 +0000

    now

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
-/*provider "aws" {
-  region = "us-east-1"
-  profile = "john"
+provider "aws" {
+  region  = "us-east-1" # Replace with your desired AWS region
+  profile = "default"   # Replace with the AWS CLI profile you want to use
 }
-*/
