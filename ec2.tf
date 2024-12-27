99df164cc67bfd66df082b62c76edcb2610005f0
jatebezi <jempire47@gmial.com>
2024-12-27 03:15:11 +0000

    now

--- ec2.tf
+++ ec2.tf
@@ -3,18 +3,8 @@ resource "aws_instance" "airbnb-web" {
   ami           =var.ami_id
 
   instance_type = var.instance_type
-
-  monitoring = true
-
-  ebs_optimized = true
-
-  root_block_device {
-
-  encrypted = true
-
-  }
   tags={
-    name="jude"
+    Name="jude"
   }
- 
 }
+
